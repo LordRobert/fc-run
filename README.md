5d3444736374b9650201c23246f313dc8ce73c7a
LordRobert <594713296@qq.com>
2017-09-09 18:33:17 +0800

    提交代码

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# fc-run
-a boot tool for ali function-compute proj
+##阿里函数计算项目的运行工具
+阿里函数计算是一个在沙盒中运行的独立工程。且函数的handler入口方法不能直接在node环境下运行。此工具模拟调用了handler的入口方法。
+
+### 注意
+此工具适用于nodejs语言开发的函数计算。
+
+### 安装
+使用npm全局安装：
+    ```
+    npm install fc-run -g
+    ```
+
+### 运行
+在函数计算代码的根目录运行：
+    ```
+    fc-run -m index.js -d data.json
+    ```
+
+### 参数说明
+-m 函数计算的入口文件，handler所在文件，默认为index.js
+-d 执行此函数的参数所在文件，默认为data.json，函数计算项目运行时会传给event
+
---
+++ fc-run.js
@@ -0,0 +1,43 @@
+var path = require('path');
+var fs = require('fs');
+
+var entry = 'index.js';
+var data = 'data.json';
+
+arguments = process.argv;
+arguments.forEach((arg, index) => {
+    if(arg === '-m') {
+        entry = arguments[index + 1] || entry;
+    }
+    if(arg === '-d') {
+        data = arguments[index + 1] || data;
+    }
+});
+
+var entryPath = path.resolve(__dirname, entry);
+var dataPath = path.resolve(__dirname, data);
+
+if(!fs.existsSync(entryPath)) {
+    console.log('函数入口文件(index.js)不存在！');
+    return;
+}
+if(!fs.existsSync(dataPath)) {
+    console.log('参数文件(daa.json)不存在！');
+    return;
+}
+
+var func = require('./' + entry);
+var data = require('./' + data);
+
+func.handler(JSON.stringify(data), {}, function(error, data) {
+    var now = new Date();
+    var rtn = now.getFullYear() + '-' + (now.getMonth() + 1) + '-' + now.getDate() + ' ' + now.getHours() + ':' + now.getMinutes() + ':' + now.getSeconds();
+    if (error) {
+        rtn += '【error】';
+    } else {
+        rtn += '【info】';
+    }
+    rtn += '函数运行结果：';
+    console.log(rtn);
+    console.log(error || data);
+});
