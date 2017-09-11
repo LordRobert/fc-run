30bc27a681d2d70abc302e4d179d8b931e76bd82
LordRobert <594713296@qq.com>
2017-09-11 10:39:14 +0800

    优化

---
+++ bin/fc-run-test.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+
+'use strict';
+var lib= require('../fc-run.js');
+var message = lib.run();
+ 
+console.log(message);
--- fc-run.js
+++ fc-run.js
@@ -1,49 +1,55 @@
-var path = require('path');
-var fs = require('fs');
+var function run () {
+    var path = require('path');
+    var fs = require('fs');
 
-var entry = 'index.js';
-var data = 'data.json';
-
-
-//test
-console.log('当前目录');
-console.log(__dirname);
+    var entry = 'index.js';
+    var data = 'data.json';
 
 
-arguments = process.argv;
-arguments.forEach((arg, index) => {
-    if(arg === '-m') {
-        entry = arguments[index + 1] || entry;
+    //test
+    console.log('当前目录');
+    console.log(__dirname);
+
+
+    arguments = process.argv;
+    arguments.forEach((arg, index) => {
+        if(arg === '-m') {
+            entry = arguments[index + 1] || entry;
+        }
+        if(arg === '-d') {
+            data = arguments[index + 1] || data;
+        }
+    });
+
+    var entryPath = path.resolve(__dirname, entry);
+    var dataPath = path.resolve(__dirname, data);
+
+    if(!fs.existsSync(entryPath)) {
+        console.log('函数入口文件(index.js)不存在！');
+        return;
     }
-    if(arg === '-d') {
-        data = arguments[index + 1] || data;
+    if(!fs.existsSync(dataPath)) {
+        console.log('参数文件(daa.json)不存在！');
+        return;
     }
-});
 
-var entryPath = path.resolve(__dirname, entry);
-var dataPath = path.resolve(__dirname, data);
+    var func = require('./' + entry);
+    var data = require('./' + data);
 
-if(!fs.existsSync(entryPath)) {
-    console.log('函数入口文件(index.js)不存在！');
-    return;
+    func.handler(JSON.stringify(data), {}, function(error, data) {
+        var now = new Date();
+        var rtn = now.getFullYear() + '-' + (now.getMonth() + 1) + '-' + now.getDate() + ' ' + now.getHours() + ':' + now.getMinutes() + ':' + now.getSeconds();
+        if (error) {
+            rtn += '【error】';
+        } else {
+            rtn += '【info】';
+        }
+        rtn += '函数运行结果：';
+        console.log(rtn);
+        return(error || data);
+    });
+
+    return 'message........';
 }
-if(!fs.existsSync(dataPath)) {
-    console.log('参数文件(daa.json)不存在！');
-    return;
-}
 
-var func = require('./' + entry);
-var data = require('./' + data);
-
-func.handler(JSON.stringify(data), {}, function(error, data) {
-    var now = new Date();
-    var rtn = now.getFullYear() + '-' + (now.getMonth() + 1) + '-' + now.getDate() + ' ' + now.getHours() + ':' + now.getMinutes() + ':' + now.getSeconds();
-    if (error) {
-        rtn += '【error】';
-    } else {
-        rtn += '【info】';
-    }
-    rtn += '函数运行结果：';
-    console.log(rtn);
-    console.log(error || data);
-});
+exports.run = run;
